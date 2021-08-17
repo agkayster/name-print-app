d56b455824c9dc5c377d7c7183a5a433bba068c4
agkayster <ejike.chiboka@gmail.com>
2021-08-17 11:46:58 +0100

    added link to zuri website

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 function App() {
 	return (
 		<div className='App'>
-			<img
-				src='	https://internship.zuri.team/static/media/logo.36d2d48a.svg'
-				alt='brand logo'
-				class='brand-logo'></img>
+			<header>
+				<figure>
+					<img
+						src='	https://internship.zuri.team/static/media/logo.36d2d48a.svg'
+						alt='brand logo'
+						class='brand-logo'></img>
+				</figure>
+			</header>
 			<h1>Ejike Chiboka</h1>
+			<footer>
+				<a
+					href='https://internship.zuri.team/'
+					target='_blank'
+					rel='noopener noreferrer'>
+					https://internship.zuri.team/
+				</a>
+			</footer>
 		</div>
 	);
 }
