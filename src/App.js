43e1a77618068cfe6558471a165939470b6f5d14
agkayster <ejike.chiboka@gmail.com>
2021-08-17 11:33:40 +0100

    added zuri logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 function App() {
 	return (
 		<div className='App'>
+			<img
+				src='	https://internship.zuri.team/static/media/logo.36d2d48a.svg'
+				alt='brand logo'
+				class='brand-logo'></img>
 			<h1>Ejike Chiboka</h1>
 		</div>
 	);
